f844d0c57312ecbf4ea78ec997f846d2e08974ba
Student <example@example.com>
2019-10-22 13:59:25 -0700

    add ids to buttons and try to show-hide some not all images

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".puppiesshowing").toggle();
-    $(".puppieshidden").toggle();
-  }
+  $("button#puppies").click(function() {
+    $("puppiesshowing").removeClass("hideAll");
+    $("puppiesshowing").toggle();
+    // $(".puppieshidden").toggle();
+  });
 
-})
+  $("button#kittens").click(function() {
+    $(".kittensshowing").toggle();
+    // $(".kittenshidden").toggle();
+  });
+
+  $("button#pandas").click(function() {
+      $(".pandasshowing").toggle();
+  });
+
+  $("button#seals").click(function() {
+      $(".sealsshowing").toggle();
+  });
+  $("button#walruses").click(function() {
+      $(".walruses").toggle();
+  });
+
+});
